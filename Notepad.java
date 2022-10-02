fce34c2576d4fc3ce9644c5458b3f2d58390bbcc
Avinash Dengani <avinashdengani123@gmail.com>
2022-10-02 18:34:29 +0530

    add: MenuBar, Menu and MenuItem in frame

--- Notepad.java
+++ Notepad.java
@@ -1,7 +1,14 @@
 public class Notepad {
     
     public static void main(String[] args) {
+
+        //Frame
         NFrame frame = new NFrame("Notepad");
+        
+        //Set a MenuBar
+        frame.setMenuBar(new NMenu());
+
+        //Set frame visible true at the end
         frame.setVisible(true);
     }
 }
