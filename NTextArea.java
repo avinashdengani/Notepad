1df0f815093f7498c9771e15ae570223dfa14b86
Avinash Dengani <avinashdengani123@gmail.com>
2022-10-02 18:52:16 +0530

    add: TextArea in Notepad

---
+++ NTextArea.java
@@ -0,0 +1,23 @@
+import java.awt.TextArea;
+import java.awt.Font;
+
+public class NTextArea extends TextArea{
+    {
+        setFont(new Font("Times New Roman", Font.PLAIN, 25));
+    }
+    NTextArea() {
+        super();
+    }
+    NTextArea(int rows, int columns) {
+        super(rows, columns);
+    }
+    NTextArea(String text) {
+        super(text);
+    }
+    NTextArea(String text, int rows, int columns) {
+        super(text, rows, columns);
+    }
+    NTextArea(String text, int rows, int columns, int scrollbars) {
+        super(text, rows, columns, scrollbars);
+    }
+}
--- Notepad.java
+++ Notepad.java
@@ -1,5 +1,6 @@
 public class Notepad {
-    
+    static NTextArea textArea;
+
     public static void main(String[] args) {
 
         //Frame
@@ -8,6 +9,10 @@ public class Notepad {
         //Set a MenuBar
         frame.setMenuBar(new NMenu());
 
+        //textArea
+        textArea = new NTextArea("", 50, 50, NTextArea.SCROLLBARS_VERTICAL_ONLY);
+        frame.add(textArea);
+        
         //Set frame visible true at the end
         frame.setVisible(true);
     }
